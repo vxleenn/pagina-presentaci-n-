528d97d1c01e4ca22874d1955bab23afef940946
vxleenn <Alivchamorroc124@umariana.edu.co>
2025-08-14 16:36:28 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,18 @@
+function home() {
+    return `
+        <h2>Inicio</h2>
+        <p>
+            Bienvenido a nuestra página sobre Redes y Sistemas Distribuidos, con un enfoque en 
+            la Interacción Humano-Computadora. Aquí exploraremos la evolución, principios, 
+            retos y perspectivas futuras de esta disciplina que conecta la tecnología con las 
+            necesidades humanas.
+        </p>
+        <p>
+            Utiliza el menú superior para navegar por las diferentes secciones y descubrir 
+            información clave sobre cómo la interacción hombre-máquina ha transformado nuestra 
+            forma de trabajar, aprender y comunicarnos.
+        </p>
+        <h2>Integrantes: Valentina Chamorro, Laura Mora, Shara Montenegro, David Lopez y Sebastian Yela</h2>
+        
+    `;
+}
